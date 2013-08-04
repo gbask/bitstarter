daf8f1db0e78d45480c0042f4b5b43de5a85ec20
gbask <gbaskett@gmail.com>
2013-08-04 12:28:07 -0700

    added contact page link

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
 var fs = require('fs');
-var buf = new Buffer(fs.readFileSync('index.html'));
+var index_buf = new Buffer(fs.readFileSync('index.html'));
+var contact_buf = new Buffer(fs.readFileSync('contact.html'));
 
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf-8'));
+  response.send(index_buf.toString('utf-8'));
+});
+
+app.get('/contact', function(request, response) {
+	response.send(contact_buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 8080;
